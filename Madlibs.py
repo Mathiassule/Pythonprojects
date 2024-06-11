29f87abf7a7dd82baf0578c7537ebdd53ff02c52
Mathiassule <mathiassule@gmail.com>
2024-06-11 15:26:07 +0100

    madlibs-branchtest commit

--- Madlibs.py
+++ Madlibs.py
@@ -8,3 +8,5 @@ famous_person = input("Famous person: ")
 madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!."
 
 print(madlib)
+
+#new branch test
