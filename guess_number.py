2233c91ac7028413e834d0ae7074d2f216c850a7
Mathiassule <mathiassule@gmail.com>
2024-06-20 14:27:33 +0100

    A guessing program using random function to predict values. Computer and user instance included

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,36 @@
 import random
 
-def guess(x):
-    random_number = random.randint(1, x)
+def guess(y):
+    myrand = random.randint(1, y)
     guess = 0
-    while guess != random_number:
-        guess = int(input(f"Guess a number between 1 and {x}\n"))
-        if guess < random_number:
-            print("Sorry, guess again. Too low,")
-        elif guess > random_number:
-            print("Sorry, guess again. Too high.")
-    print(f"Yay, congrats. You have guessed the number {random_number}")
 
-guess(10)
+    while guess != myrand:
+        guess = int(input(f"Input a number between 1 and {y}: \n"))
+        if guess < myrand:
+            print("Too low! Guess again")
+        elif guess > myrand:
+            print("Too high! Guess again")
+    print(f"Yay! You guessed the number {myrand} correctly!")
+
+
+def computguess(x):
+    low = 1
+    high = x
+    feedback = ''
+
+    while feedback != 'c':
+        if low != high:
+            matguess = random.randint(low, high)
+        else: 
+            matguess = low # low = high
+        
+        feedback = input(f"Is {matguess} too high 'h', too low 'l' or correct 'c'? \n")
+        if feedback == 'h':
+            high = matguess -1
+        elif feedback == 'l':
+            low = matguess + 1
+        
+    print(f"Yay! The computer guessed {matguess} correctly!")
+
+
+computguess(2500)
